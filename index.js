e5dcb763e9cec941c32bfaeb18f3abea21667e33
Jobsidney <jobsidney67@gmail.com>
2022-06-18 01:58:43 +0300

    commit tree bf5a2dc61fd055e0055022637b20973883f8d7c3

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+const csvDataEmployees = [
+    ["Thor", "Odinsson", "Electrical Engineer", 45],
+    ["Loki", "Laufeysson-Odinsson", "HR Representative", 35],
+    ["Natalia", "Romanov", "CEO", 150],
+    ["Darcey", "Lewis", "Intern", 15],
+    ["Jarvis", "Stark", "CIO", 125],
+    ["Anthony", "Stark", "Angel Investor", 300]
+  ]
+
+  const csvTimesIn = [
+    ["Thor", ["2018-01-01 0800", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Loki", ["2018-01-01 0700", "2018-01-02 0700", "2018-01-03 0600"]],
+    ["Natalia", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1300"]],
+    ["Darcey", ["2018-01-01 0700", "2018-01-02 0800", "2018-01-03 0800"]],
+    ["Jarvis", ["2018-01-01 0500", "2018-01-02 0500", "2018-01-03 0500"]],
+    ["Anthony", ["2018-01-01 1400", "2018-01-02 1400", "2018-01-03 1400"]]
+  ]
+
+  const csvTimesOut = [
+    ["Thor", ["2018-01-01 1600", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Loki", ["2018-01-01 1700", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Natalia", ["2018-01-01 2300", "2018-01-02 2300", "2018-01-03 2300"]],
+    ["Darcey", ["2018-01-01 1300", "2018-01-02 1300", "2018-01-03 1300"]],
+    ["Jarvis", ["2018-01-01 1800", "2018-01-02 1800", "2018-01-03 1800"]],
+    ["Anthony", ["2018-01-01 1600", "2018-01-02 1600", "2018-01-03 1600"]]
+  ]
+
+
+const employeeOdj=[]
+
+function createEmployeeRecord() {
+    const keyList=['firstName','familyName','title','payPerHour','timeInEvents','timeoutEvents']
+    const employee = {}
+    for (let j = 0; j < keyList.length; j++) {  
+        employee[keyList[j]]=csvDataEmployees[0][j]
+    }
+    return employee;
+}
+console.log(createEmployeeRecord());
+
+function createEmployeeRecords(){
+
+}
+
+function createTimeInEvent(){
+
+}
+function createTimeOutEvent(){
+
+}
+
